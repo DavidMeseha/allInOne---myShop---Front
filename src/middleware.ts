55603ae6ffaba0e578284f90f253412c8581b989
David <davidmmyh@gmail.com>
2024-10-25 19:55:35 +0300

    modifie next middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,53 +1,53 @@
-import { langs } from "@/dictionary";
-import { type MiddlewareConfig, type NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-import { User } from "./types";
+// import { langs } from "@/dictionary";
+import { type MiddlewareConfig } from "next/server";
+// import { NextResponse } from "next/server";
+// import { User } from "./types";
 
-const URL = process.env.API_BASEURL;
+// const URL = process.env.API_BASEURL;
 
-const getToken = async () => {
-  const guestUser: { user: User; token: string } = await fetch(URL + "/api/auth/guest")
-    .then((res) => res.json())
-    .catch(() => null);
-  return guestUser;
-};
+// const getToken = async () => {
+//   const guestUser: { user: User; token: string } = await fetch(URL + "/api/auth/guest")
+//     .then((res) => res.json())
+//     .catch(() => null);
+//   return guestUser;
+// };
 
-const check = async (token: string) => {
-  const guestUser: User = await fetch(URL + "/api/auth/check", {
-    headers: {
-      Authorization: "Bearer " + token
-    }
-  })
-    .then(async (res) => res.json())
-    .catch(() => null);
+// const check = async (token: string) => {
+//   const guestUser: User = await fetch(URL + "/api/auth/check", {
+//     headers: {
+//       Authorization: "Bearer " + token
+//     }
+//   })
+//     .then(async (res) => res.json())
+//     .catch(() => null);
 
-  return guestUser;
-};
+//   return guestUser;
+// };
 
-export async function middleware(req: NextRequest) {
-  let res: NextResponse = NextResponse.next();
-  const { pathname } = req.nextUrl;
-  const pathnameLang = langs.find((lang) => pathname.startsWith(`/${lang}/`) || pathname === `/${lang}`);
-  let token = req.cookies.get("access_token")?.value ?? "";
+export async function middleware() {
+  // let res: NextResponse = NextResponse.next();
+  // const { pathname } = req.nextUrl;
+  // const pathnameLang = langs.find((lang) => pathname.startsWith(`/${lang}/`) || pathname === `/${lang}`);
+  // let token = req.cookies.get("access_token")?.value ?? "";
 
-  const tokenUser = !token ? null : await check(token);
+  // const tokenUser = !token ? null : await check(token);
 
-  let user: { user: User; token: string } | null = null;
-  if (!tokenUser) {
-    user = await getToken();
-    if (user) {
-      res.cookies.set("access_token", user.token);
-    }
-  } else {
-    user = { user: tokenUser, token: token };
-  }
+  // let user: { user: User; token: string } | null = null;
+  // if (!tokenUser) {
+  //   user = await getToken();
+  //   if (user) {
+  //     res.cookies.set("access_token", user.token);
+  //   }
+  // } else {
+  //   user = { user: tokenUser, token: token };
+  // }
 
-  if (!pathnameLang) {
-    if (user) req.nextUrl.pathname = `/${user.user.language ?? "en"}${pathname}`;
-    res = NextResponse.redirect(req.nextUrl);
-  }
+  // if (!pathnameLang) {
+  //   if (user) req.nextUrl.pathname = `/${user.user.language ?? "en"}${pathname}`;
+  //   res = NextResponse.redirect(req.nextUrl);
+  // }
 
-  return res;
+  // return res;
 }
 
 export const config: MiddlewareConfig = {
